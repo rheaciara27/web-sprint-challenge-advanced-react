2574ceca72b16b00a01e6275fa897f7f6b473da7
Rhea Fleming <rheaciarafleming@gmail.com>
2024-06-26 21:21:28 -0500

    done

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,29 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AppFunctional from "./AppFunctional";
+
+test('App Functional Renders', () => {
+    render(<AppFunctional />)
+  })
+  test('two nav links', () => {
+    render(<AppFunctional />)
+    const nav = screen.queryAllByRole('nav')
+    expect(nav).toBeTruthy()
+  })
+  test('valid coordinates', () => {
+    render(<AppFunctional />)
+    const header = screen.queryByText(/coordinates/i)
+    expect(header).toBeTruthy()
+  })
+  
+  test('buttons present', () => {
+    render(<AppFunctional />)
+    const buttons = screen.queryAllByRole('button')
+    expect(buttons).toHaveLength(6)
+  })
+  
+  test('steps are counting', () => {
+    render(<AppFunctional />)
+    const movement = screen.queryByText(/you moved/i)
+    expect(movement).toBeTruthy()
+  })
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,51 +1,135 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = '';
+const initialEmail = '';
+const initialIndex = 4;
+const initialSteps = 0;
+const errMessage = ''; // the index the "B" is at
 
-export default function AppFunctional(props) {
+const URL = 'http://localhost:9000/api/result';
+
+const AppFunctional = ({ className }) => {
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [index, setIndex] = useState(initialIndex);
+  const [steps, setSteps] = useState(initialSteps);
+  const [errorMessage, setErrorMessage] = useState(errMessage);
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  function getXY() {
+  const getXY = () => {
+    const x = (index % 3) + 1;
+    const y = Math.floor(index / 3) + 1;
+    return { x, y };
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
+  };
 
-  function getXYMessage() {
+  const getXYMessage = () => {
+    const { x, y } = getXY();
+    return `(${x}, ${y})`;
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-  }
+  };
 
-  function reset() {
+  const reset = () => {
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setIndex(initialIndex);
+    setSteps(initialSteps);
+    setErrorMessage(errMessage);
     // Use this helper to reset all states to their initial values.
-  }
+  };
 
-  function getNextIndex(direction) {
+  const getNextIndex = (direction) => {
+    let newIndex = index;
+
+    switch (direction) {
+      case 'left':
+        newIndex = index % 3 !== 0 ? index - 1 : index;
+        break;
+      case 'up':
+        newIndex = index >= 3 ? index - 3 : index;
+        break;
+      case 'right':
+        newIndex = index % 3 !== 2 ? index + 1 : index;
+        break;
+      case 'down':
+        newIndex = index < 6 ? index + 3 : index;
+        break;
+      default:
+        break;
+    }
+
+    return newIndex;
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-  }
+  };
 
-  function move(evt) {
+  const move = (direction) => {
+    const newDirection = getNextIndex(direction);
+
+    if (newDirection !== index) {
+      setSteps(steps + 1);
+      setIndex(newDirection);
+      setErrorMessage('');
+    } else {
+      let errorMessage = '';
+
+      switch (direction) {
+        case 'left':
+          errorMessage = "You can't go left";
+          break;
+        case 'right':
+          errorMessage = "You can't go right";
+          break;
+        case 'up':
+          errorMessage = "You can't go up";
+          break;
+        case 'down':
+          errorMessage = "You can't go down";
+          break;
+        default:
+          errorMessage = 'Invalid move';
+      }
+
+      setErrorMessage(errorMessage);
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
-  }
+  };
 
-  function onChange(evt) {
+  const onChange = (evt) => {
+    const { id, value } = evt.target;
+    if (id === 'email') {
+      setEmail(value);
+    }
     // You will need this to update the value of the input.
-  }
+  };
 
-  function onSubmit(evt) {
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    const { x, y } = getXY();
+
+    axios
+      .post(URL, { message, email, index, steps, x, y })
+      .then((resp) => {
+        setEmail(initialEmail);
+        setErrorMessage(resp.data.message);
+        console.log(resp.data.message);
+      })
+      .catch((err) => {
+        setErrorMessage(err.response.data.message);
+      });
     // Use a POST request to send a payload to the server.
-  }
+  };
 
   return (
-    <div id="wrapper" className={props.className}>
+    <div id="wrapper" className={className}>
       <div className="info">
         <h3 id="coordinates">Coordinates (2, 2)</h3>
         <h3 id="steps">You moved 0 times</h3>
